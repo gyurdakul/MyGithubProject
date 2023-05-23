39c925cb992de4458b9c3987d1afebe5ad4419d3
gyurdakul <gokhanyurdakul17@gmail.com>
2023-05-23 22:51:17 +0300

    v2

--- src/AtmApp.java
+++ src/AtmApp.java
@@ -1,5 +1,10 @@
 public class AtmApp {
     public static void main(String[] args) {
         System.out.println("Bu bir ATM projesidir");
+        //version1
+
+        System.out.println(" Java is easy!");
+        //version 2
+
     }
 }
